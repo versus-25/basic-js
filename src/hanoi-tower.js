71f968dac268b0ea82ea258b84a1e5524cefc53a
versus-25 <ivan_schkola@mail.ru>
2021-03-20 16:27:32 +0300

    feat: implement function hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let result = 1;
+  for (let x = 2; x <= disksNumber; x++) {
+    result = result * 2 + 1
+  };
+  return {turns: result, seconds: Math.floor( 60 * 60 * result/turnsSpeed )};
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  // if (!Array.isArray()) throw new Error();
+  if (!arr.length) return [];
+  let resArr = [];
+  for (let x = 0; x <= arr.length; x++) {
+    let item = arr[x];
+    if (item === '--discard-prev' && !!resArr[resArr.length - 1]) {///////////////
+      resArr.pop();
+      continue;
+    } else if (item === '--double-prev' && !!resArr[resArr.length - 1]) {//////////////
+      resArr.push(arr[x - 1]);
+      continue;
+    } else if (item === '--double-next' && !!resArr[resArr.length]) {//////////
+      resArr.push(arr[x + 1]);
+    } else if (item === '--discard-next' && !!resArr[resArr.length]) {/////////
+      x += 1;
+      continue;
+    };
+    if (item !== '--discard-prev' && item !== '--double-prev' && item !== '--double-next' &&item !== '--discard-next') {
+      resArr.push(item)
+    };
+  };
+  return resArr
 };
+['--discard-prev', 1, 2, 3],
+['--double-prev', 1, 2, 3],
+[1, 2, 3, '--double-next'],
+[1, 2, 3, '--discard-next']
