2cafec7f337be3001a48a1b170491f13cab51ee3
versus-25 <ivan_schkola@mail.ru>
2021-03-25 21:29:49 +0300

    feat: implement simplechain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,33 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 const chainMaker = {
+  chainList: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainList.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainList.push('( ' + String(value) + ' )');
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (
+      typeof position !== 'number' ||
+      position < 1 ||
+      position > this.getLength()
+    ) {
+      this.chainList = [];
+      throw new Error();
+    }
+    this.chainList.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainList.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    return this.chainList.splice(0, this.getLength()).join('~~');
+  },
 };
 
 module.exports = chainMaker;
