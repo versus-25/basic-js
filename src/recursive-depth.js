540875f295a1a066280d9b042bd42bb56f93da7f
versus-25 <ivan_schkola@mail.ru>
2021-03-25 21:26:57 +0300

    feat: imlement chainmaker

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,8 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.list;
   }
+  calculateDepth(/* arr */) {}
 };
