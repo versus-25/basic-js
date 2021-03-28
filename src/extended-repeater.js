8a6bd8883f878a6df1ef1b71000b04feeb10792f
versus-25 <ivan_schkola@mail.ru>
2021-03-28 19:24:37 +0300

    feat: implement extended-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let repTimes = options.repeatTimes ? options.repeatTimes : 0, 
+  sep = options.separator ? options.separator : '+', 
+  add = options.addition || 
+    options.addition === false || 
+    options.addition === null ? 
+    String(options.addition) : '', 
+  addRepTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 0, 
+  addSep = options.additionSeparator ? options.additionSeparator : '|',
+  baseStr = String(str);
+
+  let baseAdd = addRepTimes === 0 ? 
+    add : 
+    Array(addRepTimes).fill(add, 0, addRepTimes).join(addSep);
+  let resStr = Array(repTimes).fill(baseStr + baseAdd, 0, repTimes).join(sep);
+  return resStr ? resStr : baseStr + baseAdd;
 };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,29 @@
 const CustomError = require('../extensions/custom-error');
 
 module.exports = class DepthCalculator {
-  constructor() {
-    this.list;
+  // constructor(arrArrays) {
+  //   //   this.arrArrays = arrArrays;
+  //   this.depth = 0;
+  //   this.counter = 0;
+  // }
+  // calculateDepth(arr) {
+  //   for (let x = 0; x < arr.length; x++) {
+  //     if (Array.isArray(arr[x])) {
+  //       this.counter++;
+  //       if (this.depth < this.counter) this.depth = this.counter;
+  //       // this.depth += this.counter;
+  //       // this.counter = 0;
+  //       this.calculateDepth(arr[x]);
+  //     }
+  //   }
+
+  //   return this.depth + 1;
+  // }
+  calculateDepth(arr) {
+    if (Array.isArray(arr)) {
+      return 1 + Math.max(...arr.map(this.calculateDepth.bind(this)));
+    } else {
+      return 0
+    }
   }
-  calculateDepth(/* arr */) {}
 };
