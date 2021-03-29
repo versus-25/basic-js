b0179ae8c63318aaa024742ae6020b955dc25c14
versus-25 <ivan_schkola@mail.ru>
2021-03-29 21:40:37 +0300

    feat: implement VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,73 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+  }
+  encrypt(strEncrypt, keyEncrypt) {
+    let str = strEncrypt.toUpperCase();
+    let key = keyEncrypt.toUpperCase();
+    let listChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let cntr = 0;
+    let lenKey = str.split('').filter((x) => listChar.includes(x)).length
+    while (lenKey > key.length) {
+      if (cntr === key.length) cntr = 0;
+      key += key[cntr];
+      cntr++;
+    }
+
+    let res = [];
+    let x_key = 0;
+    let x_str = 0;
+    while (x_str < str.length) {
+      let char = str[x_str]
+      if (!listChar.includes(char)) {
+        x_key--;
+        res.push(char);
+      } else {
+        let y = listChar.indexOf(key[x_key]);
+        let x = listChar.indexOf(char);
+        res.push(listChar[(y + x) % 26])
+      }
+      x_key++;
+      x_str++;
+    }
+    return this.direct ? res.join('') : res.reverse.join('')
+  }
+  decrypt(strDecrypt, keyDecrypt) {
+    let str = strDecrypt.toUpperCase();
+    let key = keyDecrypt.toUpperCase();
+    let listChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let cntr = 0;
+    let lenKey = str.split('').filter((x) => listChar.includes(x)).length
+    while (lenKey > key.length) {
+      if (cntr === key.length) cntr = 0;
+      key += key[cntr];
+      cntr++;
+    }
+
+    let res = [];
+    let x_key = 0;
+    let x_str = 0;
+    while (x_str < str.length) {
+      let char = str[x_str]
+      if (!listChar.includes(char)) {
+        x_key--;
+        res.push(char);
+      } else {
+        let y = listChar.indexOf(key[x_key]);
+        let x = listChar.indexOf(char);
+        res.push(listChar[(x + 26 - y) % 26])
+      }
+      x_key++;
+      x_str++;
+    }
+    // console.log(
+    //   [strDecrypt, 
+    //   keyDecrypt,
+    //   this.direct ? res.join('') : res.reverse.join('')]
+    // )
+    return this.direct ? res.join('') : res.reverse.join('')
   }
 }
 
